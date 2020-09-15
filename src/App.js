35ecad0ec0f9c0f02a935c314a0110be23abeae6
John Ludena <johnludena@gmail.com>
2020-09-15 07:56:46 -0500

    Live editor working (kinda). 09/15:  🍅🍅🍅🍅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import GameGrid from './components/GameGrid';
+import CodeEditor from './components/CodeEditor';
 import './App.scss';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       
       <div className="left-col col">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae, at saepe eaque aperiam soluta culpa doloribus incidunt officiis, corrupti vel molestiae hic blanditiis, natus nesciunt! Adipisci aliquid architecto a quod.</p>
+        <CodeEditor />
       </div>
       <div className="right-col col">
         <GameGrid />
---
+++ src/components/CodeEditor.js
@@ -0,0 +1,147 @@
+import React from "react";
+// import { render } from "@testing-library/react";
+
+import { Controlled as CodeMirror } from 'react-codemirror2'; // CodeMirror React wrapper
+
+// CodeMirror CSS imports
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/javascript/javascript';
+
+
+class CodeEditor extends React.Component {
+  constructor() {
+    super();
+    this.iframe = React.createRef(); // new React refs format;
+
+    this.state = {
+      html: `<button>click me</button>`,
+      css: `
+      button {
+        padding: 10px 20px;
+      background: blue;
+      color: white;
+    }
+
+    button.active {
+        background: red;
+    }
+      `,
+      js: `
+      let button = document.querySelector('button');
+
+button.addEventListener('click', ()=> {
+	button.classList.add('active');
+});`,
+    };
+  }
+
+  componentDidUpdate() {
+    
+    // trigger an update when user adds new code
+    this.runCode();
+  }
+
+  componentDidMount() {
+
+    // initial runCode();
+    this.runCode();
+  }
+
+  runCode = () => {
+    const { html, css, js } = this.state;
+
+    const iframe = this.iframe.current;
+    const document = iframe.contentDocument;
+    const documentContents = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <style>
+          ${css}
+        </style>
+      </head>
+      <body>
+        ${html}
+
+        <script type="text/javascript">
+          ${js}
+        </script>
+      </body>
+      </html>
+    `;
+
+    document.open();
+    document.write(documentContents);
+    document.close();
+  };
+
+  render() {
+    const { html, js, css } = this.state;
+
+    const codeMirrorOptions = {
+        theme: 'material',
+        lineNumbers: true,
+        scrollbarStyle: null,
+        lineWrapping: true,
+    };
+
+    return (
+      <div className="code-editor-wrapper">
+        <section className="playground">
+          <div className="code-editor html-code">
+            <div className="editor-header">HTML</div>
+            <CodeMirror
+              value={html}
+              options={{
+                mode: "htmlmixed",
+                ...codeMirrorOptions,
+              }}
+              onBeforeChange={(editor, data, html) => {
+                this.setState({ html });
+              }}
+            />
+          </div>
+          <div className="code-editor css-code">
+            <div className="editor-header">CSS</div>
+            <CodeMirror
+              value={css}
+              options={{
+                mode: "css",
+                ...codeMirrorOptions,
+              }}
+              onBeforeChange={(editor, data, css) => {
+                this.setState({ css });
+              }}
+            />
+          </div>
+          <div className="code-editor js-code">
+            <div className="editor-header">JavaScript</div>
+            <CodeMirror
+              value={js}
+              options={{
+                mode: "javascript",
+                ...codeMirrorOptions,
+              }}
+              onBeforeChange={(editor, data, js) => {
+                this.setState({ js });
+              }}
+            />
+          </div>
+        </section>
+        <section className="result">
+          <iframe title="result" className="iframe" ref={this.iframe} />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default CodeEditor;
