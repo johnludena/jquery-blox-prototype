f8a25f678fdcfb3d4120aea0ed33fe572a6d18fc
John Ludena <johnludena@gmail.com>
2020-09-26 16:53:51 -0500

    Added block styles for button and div elements. Also linked CSS stylesheet for all block elements instead of using a string inside component for better organization.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import GameGrid from './components/GameGrid';
 import CodeEditor from './components/CodeEditor';
 import './App.scss';
 
-let startHtml = `<button>hi</button>`;
+let startHtml = `<button class="block">btn</button>
+<div class="block">div</div>`;
 let startCss = `button {
   border: 2px dashed #de7186;
 }
@@ -38,6 +39,8 @@ class App extends React.Component {
       <div className="App">
         
         <div className="left-col col">
+          <h1>Lesson 1</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde minus eos hic consectetur dolorum facere maxime nihil repellendus facilis nam, eveniet fugit consequuntur ipsa harum atque rerum maiores iusto est.</p>
           <CodeEditor data={this.state} refreshCode={this.updateUi} />
         </div>
         <div className="right-col col">
--- src/components/GameGrid.js
+++ src/components/GameGrid.js
@@ -1,24 +1,7 @@
 import React from "react";
 import successSound from '../audio/success.wav';
 
-console.log({successSound})
-
-let engineCSS = `body > * {
-  background: #444444;
-      width: 125px;
-      height: 125px;
-      margin: 10px;
-      transition: 0.2s ease-out all;
-      border: 2px solid #444444;
-      color: white;
-}
-
-button {
-  transition: .2s ease-out all;
-}
-`
-
-let finalJs = `var button = document.querySelector('button');`
+let finalJs = `var button = document.querySelector('button')`
 
 
 class GameGrid extends React.Component {
@@ -59,10 +42,8 @@ class GameGrid extends React.Component {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-        <style>
-          ${engineCSS}
-        </style>
+        <title>Your Awesome Game!</title>
+        <link rel="stylesheet" href="${process.env.PUBLIC_URL + '/GameBlocks.css'}">
         <style>
           ${css}
         </style>
@@ -86,45 +67,7 @@ class GameGrid extends React.Component {
     return (
       <div id="GameGrid">
         <iframe title="result" className="iframe" ref={this.iframe} />
-        {/* <div className="row">
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-        </div>
-
-        <div className="row">
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-        </div>
-
-        <div className="row">
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block pink"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-        </div>
-
-        <div className="row">
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-        </div>
-
-        <div className="row">
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-          <div className="block"></div>
-        </div> */}
+       
         <audio src={successSound} ref={this.audioFile}></audio>
 
       </div>
