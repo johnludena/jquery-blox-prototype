49458f3d3fc90510e0887ffd0d5bea069e405dfe
John Ludena <johnludena@gmail.com>
2020-10-19 19:06:24 -0500

    VICTORY IS MINE! Finally able to correctly update state lessons array with code payload.

--- src/redux/reducers/lessons.js
+++ src/redux/reducers/lessons.js
@@ -4,7 +4,7 @@ import { CODE_UPDATED, LESSON_PASSED, LESSON_SUBMITTED  } from "../actions";
 // import { ADD_USER, TOGGLE_TODO } from "../actionTypes";
 
 const initialState = {
-  lessons: lessonsArr // ['lesson1', 'lesson2', 'lesson3]
+  lessons: lessonsArr
 };
 
 export default function(state = initialState, action) {
@@ -30,10 +30,23 @@ export default function(state = initialState, action) {
     case CODE_UPDATED: {
       const { codeType, content, lessonIndex } = action.payload; // destructure payload properties
 
-      let newState = { ...state }
-      
-      return newState;
+      let newState =  {
+        ...state,
 
+        // map through all lessons and return new array with modified object
+        lessons: state.lessons.map((lesson, index)=> {
+          if (index === lessonIndex) {
+            return {...lesson, [codeType]: content }
+          }
+          else {
+            return lesson;
+          }
+        })  
+      }
+
+      console.log('newState =>', newState)
+
+      return newState;
     }
     
     default:
