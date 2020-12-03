def226be51e4a404763a07af0635a8e61fcc5dd7
John Ludena <johnludena@gmail.com>
2020-12-02 19:48:37 -0600

    First successful part of testing with Mocha and Chai inside iframe.

--- src/components/Validator.js
+++ src/components/Validator.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createPortal } from "react-dom";
-
+import IFrame from "./IFrame";
 
 class Validator extends React.Component {
 
@@ -10,52 +10,80 @@ class Validator extends React.Component {
 		this.iframe = React.createRef();
 	}
 
+	componentDidMount = () => {
+		this.renderIframe();
+	}
+
+	renderIframe = () => {
+
+		const iframe = this.iframe.current;
+    const document = iframe.contentDocument;
+
+		const documentContents = `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Your Awesome Game!</title>
+      
+      </head>
+			<body>
+				<h1>Heading test</h1>
+				<div id="mocha"></div>
+				<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/chai/3.5.0/chai.js"></script>
+				<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mocha/2.4.5/mocha.js"></script>
+				<script type="text/javascript">
+				// Setup
+				mocha.setup('bdd');
+
+				console.log(chai)
+				
+				// Let's test this function
+				const addr = (a, b) =>  a + b;
+				
+				// Assertion made below
+				describe('Addr adds two numbers', () => {
+				
+				  it('Adds two numbers', function() {
+				    chai.assert.equal(addr(2, 2), 3);
+				  });
+				
+				  it('Lesson 2 Tests', function() {
+				
+				    chai.expect("John").to.be.a('string'); // Recommended
+				
+				  });
+				
+				});
+				
+				mocha.run();
+				
+				</script>
+				
+      </body>
+      </html>
+    `;
+
+    document.open();
+    document.write(documentContents);
+    document.close();
+	}
+
   render = () => {
+
     return (
-      <div className="Validator">
-        <iframe name="GameGridIframe" className="ValidatorIframe" ref={this.iframe}>
-					<h1>Heading 1</h1>
-				</iframe>
+      <div className="ValidatorIframe">
+        <iframe
+					title="Okama Game Sphere"
+					name="GameGridIframe"
+					id="mySpecialId"
+					className="iframe"
+					ref={this.iframe}
+					/>
       </div>
     );
   }
 }
 
 export default Validator;
-
-// import { expect, assert } from 'chai';
-// import mocha from 'mocha';
-
-// // Lesson 1: Variables for assertion testing
-// let age = 36;
-// let testNumber = 2;
-
-// // Lesson 2: jQuery selection
-// let userJsStr = "var johnnyboy = 5; alert(johnnyboy)";
-// eval(userJsStr);
-
-// // ================
-// // Setup
-// mocha.setup('bdd');
-
-// // Let's test this function
-// const addr = (a, b) =>  a + b;
-
-// // Assertion made below
-// describe('Addr adds two numbers', () => {
-
-//   it('Adds two numbers', function() {
-//     assert.equal(addr(1, 2), 3);
-//   });
-
-//   it('Lesson 2 Tests', function() {
-
-//     expect("John").to.be.a('string'); // Recommended
-
-//     // Uncomment below to see Failing test
-//     // assert.equal(addr(3, 3), -6);
-//   });
-
-// });
-
-// mocha.run();
