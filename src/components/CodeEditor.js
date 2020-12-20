9d9782a4cd3db64475d24499aa64f86f1204bd2a
John Ludena <johnludena@gmail.com>
2020-12-20 16:36:30 -0600

    Tabbing system for JS and results tab panes complete.

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -3,8 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { UnControlled as CodeMirror } from "react-codemirror2"; // CodeMirror React wrapper
-import Validator from './Validator';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
+
+// App Components
+import Validator from "./Validator";
 
 // CodeMirror CSS imports
 import "codemirror/lib/codemirror.css";
@@ -16,6 +22,15 @@ class CodeEditor extends React.Component {
     super(props);
 
     this.lessonIndex = this.props.lessonKey;
+    this.editorTab = React.createRef();
+    this.resultsTab = React.createRef();
+
+    this.state = {
+      codeEditorTab: "active",
+      resultsTab: "",
+      codeEditorTabContent: "active",
+      resultsTabContent: "",
+    };
   }
 
   componentDidUpdate = () => {
@@ -30,44 +45,47 @@ class CodeEditor extends React.Component {
     let lessonIndex = this.lessonIndex;
 
     this.props.dispatch({
-      type: 'CODE_UPDATED',
+      type: "CODE_UPDATED",
       payload: {
-        codeType, content, lessonIndex
-      }
+        codeType,
+        content,
+        lessonIndex,
+      },
     });
 
     // switch lessonSubmitted status to off when re-editing to prevent re-render of validation
     // IS THIS EVEN GETTING UPDATED?
     let lessonSubmittedStatus = false;
     this.props.dispatch({
-      type: 'LESSON_SUBMITTED',
+      type: "LESSON_SUBMITTED",
       payload: {
-         lessonSubmittedStatus,
-         lessonIndex,
-      }
-    }); 
+        lessonSubmittedStatus,
+        lessonIndex,
+      },
+    });
   };
 
   handleCodeSubmission = () => {
-    
     let lessonIndex = this.lessonIndex;
     let lessonSubmittedStatus = true;
 
-    console.log('handleCodeSubmission')
+    console.log("handleCodeSubmission");
 
     this.props.dispatch({
-      type: 'LESSON_SUBMITTED',
+      type: "LESSON_SUBMITTED",
       payload: {
-         lessonSubmittedStatus,
-         lessonIndex,
-      }
+        lessonSubmittedStatus,
+        lessonIndex,
+      },
     });
   };
 
   handleTabChange = (event) => {
-    console.log('handleTabChange');
-    console.log(event)
-  }
+    event.preventDefault();
+    console.log("handleTabChange");
+
+    console.log(event);
+  };
 
   render = () => {
     this.setData();
@@ -84,7 +102,10 @@ class CodeEditor extends React.Component {
     const showNextButton = () => {
       if (this.lessonData.lessonPassed) {
         return (
-          <Link className="btn secondary-btn" to={`/${this.props.lessonKey + 2}`}>
+          <Link
+            className="btn secondary-btn"
+            to={`/${this.props.lessonKey + 2}`}
+          >
             Go to next challenge
           </Link>
         );
@@ -93,36 +114,50 @@ class CodeEditor extends React.Component {
       return;
     };
 
+    const resultsIcon = () => {
+      if (this.lessonData.lessonSubmitted && this.lessonData.lessonPassed) {
+        return <FontAwesomeIcon icon={faCheckCircle} />;
+      } else if (this.lessonData.lessonSubmitted && !this.lessonData.lessonPassed) {
+        return <FontAwesomeIcon icon={faTimesCircle} />;
+      } else {
+        return '';
+      }
+    }
+
     return (
       <div className="CodeEditor">
-        <ul className="tabs">
-          <li><a href="#tab-code-editor" className="active" onClick={this.handleTabChange}>Javascript</a></li>
-          <li><a href="#tab-results" onClick={this.handleTabChange}>View results</a></li>
-        </ul>
-        <div id="tab-code-editor">
-          <CodeMirror
-            value={js}
-            options={{
-              mode: "javascript",
-              ...codeMirrorOptions,
-            }}
-            onChange={(editor, data, js) => {
-              this.handleCodeUpdates("js", js);
-            }}
-          />
+        <Tabs forceRenderTabPanel="true">
+          <TabList>
+            <Tab>Javascript</Tab>
+            <Tab>{resultsIcon()} View Results</Tab>
+          </TabList>
 
-          <div className="flex justify-content-between bg-purple-pale padding-30">
-            <button className="primary-btn btn" onClick={this.handleCodeSubmission}>Submit code</button>
-            {showNextButton()}
-          </div>
-        </div>
+          <TabPanel>
+            <CodeMirror
+              value={js}
+              options={{
+                mode: "javascript",
+                ...codeMirrorOptions,
+              }}
+              onChange={(editor, data, js) => {
+                this.handleCodeUpdates("js", js);
+              }}
+            />
 
-        <div id="tab-results">
-          <Validator lessonKey={this.props.lessonKey} />
-        </div>
+            <div className="flex justify-content-between bg-purple-pale padding-30">
+              <button className="primary-btn btn" onClick={this.handleCodeSubmission}>
+                Submit code
+              </button>
+              {showNextButton()}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <Validator lessonKey={this.props.lessonKey} />
+          </TabPanel>
+        </Tabs>
       </div>
     );
-  }
+  };
 }
 
 const mapStateToProps = function (state) {
