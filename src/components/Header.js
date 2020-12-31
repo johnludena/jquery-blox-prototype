f8ceb1cbece55630f201194db958d31875d45f9b
John Ludena <johnludena@gmail.com>
2020-12-31 11:26:52 -0600

    Rough draft tiny updates. Top navigation component updated to prevent back or next on first and last lessons.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,47 @@ import { connect } from "react-redux";
 class Header extends React.Component {
   constructor(props) {
     super(props);
+  }
 
-    console.log(this.props.lessonKey)
+  setPrevButton = () => {
+
+    let currentLessonIndex = this.props.lessonKey;
+    
+    // disable prev button on first lesson
+    if (currentLessonIndex === 0) {
+      return <Link className="btn secondary-btn disabled" to={`/${this.props.lessonKey}`}>Previous</Link>
+    } else {
+      return <Link className="btn secondary-btn" to={`/${this.props.lessonKey}`}>Previous</Link>
+    }  
+  }
+
+  setNextButton = () => {
+
+    let totalLessons = this.props.lessons.length;
+    let currentLessonIndex = this.props.lessonKey;
+
+    console.log({totalLessons, currentLessonIndex})
+
+    
+    // disable next button if we are already on last lesson
+    if (currentLessonIndex === totalLessons - 1) {
+      return <Link className="btn secondary-btn margin-left-20 disabled" to={`/${this.props.lessonKey + 2}`}>Next</Link>
+    } else {
+      return <Link className="btn secondary-btn margin-left-20" to={`/${this.props.lessonKey + 2}`}>Next</Link>
+    }  
   }
 
   render = () => {
+
+    let prevButton = this.setPrevButton();
+    let nextButton = this.setNextButton();
+
     return (
         <div className="Header flex justify-content-between">
             <a id="logo" href="/">jQueryBlox</a>
             <div className="top-navigation">
-                <Link className="btn secondary-btn" to={`/${this.props.lessonKey}`}>Previous</Link>
-                <Link className="btn secondary-btn margin-left-20" to={`/${this.props.lessonKey + 2}`}>Next</Link>
-
+                {prevButton}
+                {nextButton}
             </div>
         </div>
     );
