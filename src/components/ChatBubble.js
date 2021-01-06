14c003e6422a8055f4ffe7ac536b0121dbb7bbe5
John Ludena <johnludena@gmail.com>
2021-01-05 20:31:00 -0600

    ChatBubble component toggle done. Needs final styling.

--- src/components/ChatBubble.js
+++ src/components/ChatBubble.js
@@ -4,21 +4,31 @@ import { connect } from "react-redux";
 class ChatBubble extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      displayMessages: false,
+    }
+  }
+
+  openMessages = () => {
+    this.setState({
+      displayMessages: !this.state.displayMessages,
+    })
   }
 
   render = () => {
 
     return (
         <div className="ChatBubble">
-            <div className="messages-wrapper">
+            <div className={`${this.state.displayMessages ? 'active' : ''} messages-wrapper`}>
               <ul>
                 <li>Aenean sed adipiscing diam donec adipiscing tristique risus nec. Dolor morbi non arcu risus quis varius quam. </li>
-                <li>Magnis dis parturient montes nascetur.</li>
+                <li>Magnis dis parturient <a href="#">montes</a> nascetur.</li>
                 <li>Nascetur ridiculus mus mauris vitae ultricies. Velit aliquet sagittis id consectetur purus ut. </li>
               </ul>
             </div>
-            <div className="chat-bubble-wrapper">
-              <div className="bubble"></div>
+            <div className={`${this.state.displayMessages ? 'active' : ''} chat-bubble-wrapper`}>
+              <button onClick={this.openMessages} className="bubble"></button>
               <span className="alerts">2</span>
             </div>
         </div>
