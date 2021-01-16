d6bb95bc6b1d38b34067d681637973203dbb8ed8
John Ludena <johnludena@gmail.com>
2021-01-16 14:22:36 -0600

    Adding test MailChimp subscribe form.

--- src/components/RightCol.js
+++ src/components/RightCol.js
@@ -4,6 +4,7 @@ import GameGrid from './GameGrid';
 import Modal from 'react-modal';
 import ChatBubble from './ChatBubble';
 import FeedbackForm from './FeedbackForm';
+import Mailchimp from 'react-mailchimp-form';
 
 const customModalStyles = {
   content : {
@@ -50,15 +51,39 @@ class RightCol extends React.Component {
             <GameGrid lessonKey={this.props.lessonKey} />
         </div>
 
-        <Modal isOpen={this.state.modalActiveStatus} style={customModalStyles} onRequestClose={this.closeModal}>
+        {/* <Modal isOpen={this.state.modalActiveStatus} style={customModalStyles} onRequestClose={this.closeModal}>
             <button onClick={this.closeModal}>close modal</button>
             <h1>Modal content test</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa repellendus culpa exercitationem. Exercitationem explicabo sed quidem? Hic amet, reiciendis omnis, deleniti odio, voluptatem eos totam corporis ipsum illo fugit vel!</p>
 
             <FeedbackForm />
+        </Modal> */}
+
+        <Modal isOpen={this.state.modalActiveStatus} style={customModalStyles} onRequestClose={this.closeModal}>
+            <button onClick={this.closeModal}>close modal</button>
+            <h1>MailChimp Signup Form</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa repellendus culpa exercitationem. Exercitationem explicabo sed quidem? Hic amet, reiciendis omnis, deleniti odio, voluptatem eos totam corporis ipsum illo fugit vel!</p>
+
+            <Mailchimp
+              action='https://boldpixelmedia.us5.list-manage.com/subscribe/post?u=d6c6ce7da87357b90e9859e81&amp;id=2091bdd722'
+              fields={[
+                {
+                  name: 'FNAME',
+                  placeholder: 'Your first name',
+                  type: 'text',
+                  required: false
+                },
+                {
+                  name: 'EMAIL',
+                  placeholder: 'Email',
+                  type: 'email',
+                  required: true
+                }
+              ]}
+              />
         </Modal>
 
-        {/* <button onClick={this.openModal}>Open Modal</button> */}
+        <button onClick={this.openModal}>Open Modal</button>
 
         <ChatBubble />
       </div>
